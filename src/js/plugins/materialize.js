6ea1d1279a4d14f7c54f00e3ba98e339081fea12
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2022-01-31 04:13:38 +0200

    создание UI карточки, подключение autocomplete, datepicker и двух кнопок

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -9,3 +9,27 @@ M.FormSelect.init(select);
 export function getSelectInstance(elem) {
     return M.FormSelect.getInstance(elem);
 }
+
+// Init Autocomplete
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+    data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250',
+  },
+});
+
+export function getAutocompleteInstance(elem) {
+    return M.Autocomplete.getInstance(elem);
+}
+
+// Init Datepickers
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+    showClearBtn: true,
+});
+
+export function getDatePickerInstance(elem) {
+    return M.Datepicker.getInstance(elem);
+}
