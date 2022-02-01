c9142224b8dcbfe60a79f3280714c8e8577052a8
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2022-02-01 18:57:58 +0200

    добавил класс управления формой, получение элементов формой, реализовал методы для взаимодействия с этими элементами

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,41 @@
 import '../css/style.css';
 import './plugins';
 import locations from "./store/locations";
+import formUI from './views/form';
+import currencyUI from './views/currency';
 
-locations.init().then(res => {
-    console.log(res);
-    console.log(locations);
-    console.log(locations.getCitiesByCountryCode('UA'));
+
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+    const form = formUI.form;
+
+    // Events
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onFormSubmit();
+    });
+
+    // Handlers
+    async function initApp() {
+        await locations.init();
+        formUI.setAutocompleteDate(locations.shortCitiesList);
+    }
+
+    async function onFormSubmit() {
+        // собрать данные из инпутов
+        const origin = locations.getCityCodeByKey(formUI.originValue);
+        const destination = locations.getCityCodeByKey(formUI.destinationValue);
+        const depart_date = formUI.departDateValue;
+        const return_date = formUI.returnDateValue;
+        const currency = currencyUI.currencyValue;
+        // CODE, CODE, 2022-02, 2022-06
+        console.log(origin, destination, depart_date, return_date);
+        await locations.fetchTickets({
+            origin,
+            destination,
+            depart_date,
+            return_date,
+            currency,
+        });
+    }
 });
